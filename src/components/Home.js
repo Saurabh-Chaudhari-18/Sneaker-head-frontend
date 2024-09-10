0d5b404030f282852cad8766818b08b585424a12
Saurabh Chaudhari <139246425+Saurabh-Chaudhari-18@users.noreply.github.com>
2024-09-10 16:27:33 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,21 +1,40 @@
-// src/pages/Home.js
-import React from "react";
-import Header from "../components/Header";
+// // src/pages/Home.js
+// import React from "react";
+// import Header from "../components/Header";
 
-import ImageGrid from "../components/ImageGrid"; // Import the ImageGrid component
-import CarouselCard from "../components/CarouselCard";
-import ShoeInfoSection from "../components/ShoeInfoSection";
-// import ProductGallery from './PromoAndProducts';
+// import ImageGrid from "../components/ImageGrid"; // Import the ImageGrid component
+// import CarouselCard from "../components/CarouselCard";
+// import ShoeInfoSection from "../components/ShoeInfoSection";
+// // import ProductGallery from './PromoAndProducts';
 
-const Home = () => {
-  return (
-    <>
-      <CarouselCard />
-      {/* <ShoeInfoSection /> */}
-      {/* <ProductGallery/> */}
+// const Home = () => {
+//   return (
+//     <>
+//       <CarouselCard />
+//       {/* <ShoeInfoSection /> */}
+//       {/* <ProductGallery/> */}
      
-    </>
+//     </>
+//   );
+// };
+
+// export default Home;
+import React from 'react';
+import './Home.css';
+
+import ImageSlider from '../components/ImageSlider';
+
+
+function Home() {
+  return (
+    <div className="home">
+      <h2>Welcome to My Sneaker Site</h2>
+      <p>Discover the best products and shop with ease.</p>
+      <div className="hero">
+        <ImageSlider></ImageSlider>
+      </div>
+    </div>
   );
-};
+}
 
 export default Home;
